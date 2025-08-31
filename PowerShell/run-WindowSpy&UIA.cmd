c60b303dc28375e942fdf98f462ea106ee51e0dc
Adam <adambvista@yahoo.com>
2025-08-30 21:26:29 -0700

    updated the ahk location to be stored in a variable

--- PowerShell/run-WindowSpy&UIA.cmd
+++ PowerShell/run-WindowSpy&UIA.cmd
@@ -1,3 +1,4 @@
 @ECHO OFF
-start "" "%CD:~0,3%AutoHotkey_2.0.19\AutoHotkey64.exe" %CD:~0,3%AutoHotkey_2.0.19\Script\Lib\UIA.ahk
-start "" "%CD:~0,3%AutoHotkey_2.0.19\AutoHotkey64.exe" %CD:~0,3%AutoHotkey_2.0.19\UX\WindowSpy.ahk
+set "ahkExe=%CD:~0,3%AutoHotkey_2.0.19\AutoHotkey64.exe"
+start "" "%ahkExe%" %CD:~0,3%AutoHotkey_2.0.19\Script\Lib\UIA.ahk
+start "" "%ahkExe%" %CD:~0,3%AutoHotkey_2.0.19\UX\WindowSpy.ahk
