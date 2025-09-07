4620ef3b1bc8b5f4cb9108765faa23b1a368d99d
Adam <adambvista@yahoo.com>
2025-09-07 00:24:46 -0700

    fix echo not displaying a new line

--- PowerShell/run-enterprise-auto-installer.cmd
+++ PowerShell/run-enterprise-auto-installer.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 start "" "%CD:~0,3%AutoHotkey_2.0.19\AutoHotkey64.exe" %CD:~0,3%AutoHotkey_2.0.19\Script\Auto_Install_Windows_Enterprise.ahk
-@ECHO ""
+@ECHO.
 @ECHO Hold on to 'Ctrl + Alt + S' to stop the automated installer
 @ECHO Press 'Ctrl + Alt + Z' to resume the automated installer
 @ECHO Press 'Ctrl + Alt + X' to terminate the automated installer
